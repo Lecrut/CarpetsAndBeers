babab236fabc2297c794e383f6287e2a465498cf
Yibux <240810@edu.p.lodz.pl>
2024-04-04 15:45:23 +0200

    Add new files and configurations for BeersAndCarpets project

---
+++ backend/src/main/kotlin/beer/BeersAndCarpets/BeersAndCarpetsApplication.kt
@@ -0,0 +1,11 @@
+package beer.BeersAndCarpets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeersAndCarpetsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeersAndCarpetsApplication>(*args)
+}
---
+++ backend/src/test/kotlin/beer/BeersAndCarpets/BeersAndCarpetsApplicationTests.kt
@@ -0,0 +1,13 @@
+package beer.BeersAndCarpets
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeersAndCarpetsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
