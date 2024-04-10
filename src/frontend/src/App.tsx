64b6f7a74432be4751cb9aa83ae87a4a2bcb7b6e
Mateusz Milczarek <milczary.mm@gmail.com>
2024-04-10 20:49:57 +0200

    Configured eslint config and auto-format on save

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Button } from '@mui/material'
 import './App.css'
 
 function App() {
-
   return (
     <>
       <Button variant="outlined">Test</Button>
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
+
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
