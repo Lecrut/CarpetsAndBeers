f2e9b12ad36d1625301e84d5618e609ea4b5d984
Yibux <240810@edu.p.lodz.pl>
2024-06-16 19:55:38 +0200

    updating scripts

--- createImageScript.bat
+++ createImageScript.bat
@@ -1,12 +1,17 @@
 @echo off
+call buildScript.bat
 
-:: Step 1: Remove the existing Docker image
+del server.
+
+docker rm -f EBiznesServer
+
+REM Step 1: Remove the existing Docker image
 docker rmi server-to-login-e-biznes
 
-:: Step 2: Build the Docker image
+REM Step 2: Build the Docker image
 docker build -t server-to-login-e-biznes .
 
-:: Step 3: Save the Docker image to a tar file
+REM Step 3: Save the Docker image to a tar file
 docker save -o server.tar server-to-login-e-biznes
 
 echo Docker image server-to-login-e-biznes has been built and saved to server.tar
